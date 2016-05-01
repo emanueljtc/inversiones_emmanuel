437712ed39f92bcfad27f833e20f1c845de0f08e
emanueljtc <emanueljtc@gmail.com>
2016-05-01 14:53:26 -0400

    cambios gitignore

--- app/Config/database.php
+++ app/Config/database.php
@@ -71,7 +71,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'ema18787',
 		'database' => 'arte_cristal',
 		'prefix' => '',
 		//'encoding' => 'utf8',
