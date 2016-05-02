11d938ebdc8e73e5f4999f49136975fd18a7fcb5
emanueljtc <emanueljtc@gmail.com>
2016-05-01 22:58:30 -0400

    nuevas tables en la BD

--- arte_cristal.sql
+++ arte_cristal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 30-04-2016 a las 23:54:03
+-- Tiempo de generación: 01-05-2016 a las 22:56:02
 -- Versión del servidor: 5.5.49-0ubuntu0.14.04.1
 -- Versión de PHP: 5.5.9-1ubuntu4.16
 
@@ -23,6 +23,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `dh_extras`
+--
+
+CREATE TABLE IF NOT EXISTS `dh_extras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `personal_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `n_day` int(11) DEFAULT NULL,
+  `n_hour` int(11) DEFAULT NULL,
+  `amount` decimal(10,0) NOT NULL,
+  `payment_day` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `personals`
 --
 
@@ -36,12 +53,22 @@ CREATE TABLE IF NOT EXISTS `personals` (
   `born_date` date NOT NULL,
   `sex` int(1) NOT NULL,
   `age` int(3) NOT NULL,
-  `cell_phone` int(20) NOT NULL,
-  `phone` int(20) NOT NULL,
+  `cell_phone` varchar(20) NOT NULL,
+  `phone` varchar(20) NOT NULL,
   `email` varchar(250) NOT NULL,
   `address` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `personals`
+--
+
+INSERT INTO `personals` (`id`, `position_id`, `date_reg`, `dni`, `name`, `last_name`, `born_date`, `sex`, `age`, `cell_phone`, `phone`, `email`, `address`) VALUES
+(1, 1, '2016-05-01', 18971787, 'Emanuel', 'Torres', '1996-07-01', 0, 25, '04165615973', '02464315404', 'emanueljtc@gmail.com', 'Las Palmas'),
+(2, 1, '2016-05-01', 123213245, 'Pedro', 'Perez', '2016-05-01', 0, 34, '0412234567', '02164315404', 'j@h.com', 'Peralta'),
+(3, 1, '0000-00-00', 19221592, 'Luis', 'Mendez', '1989-04-12', 0, 27, '04120486034', '0246000888', 'luismedezinfomatica@gmail.com', 'Pariapan Bloque 2'),
+(4, 1, '0000-00-00', 123213245, 'Emelina', 'Perez', '2016-05-01', 0, 34, '0412234567', '02164315404', 'j@h.com', 'Peralta');
 
 -- --------------------------------------------------------
 
@@ -52,8 +79,9 @@ CREATE TABLE IF NOT EXISTS `personals` (
 CREATE TABLE IF NOT EXISTS `positions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `position` varchar(255) NOT NULL,
-  `day_hour` decimal(10,0) NOT NULL,
+  `hour_worked` decimal(10,0) NOT NULL,
   `time_value` decimal(10,0) NOT NULL,
+  `daily_salary` decimal(10,0) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -61,8 +89,27 @@ CREATE TABLE IF NOT EXISTS `positions` (
 -- Volcado de datos para la tabla `positions`
 --
 
-INSERT INTO `positions` (`id`, `position`, `day_hour`, `time_value`) VALUES
-(1, 'Gerente', 8, 125);
+INSERT INTO `positions` (`id`, `position`, `hour_worked`, `time_value`, `daily_salary`) VALUES
+(1, 'Gerente', 8, 125, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wakes`
+--
+
+CREATE TABLE IF NOT EXISTS `wakes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `personal_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `dh_extra_id` int(11) NOT NULL,
+  `payment_day` date NOT NULL,
+  `amount` decimal(10,0) NOT NULL,
+  `payment_type` varchar(255) NOT NULL,
+  `start` date NOT NULL,
+  `end` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
